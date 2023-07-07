0ecb71af8b87d33bb46b2f61038e0c8cdea38b82
notMrpod <darsh.pod@gmail.com>
2023-07-06 22:18:23 -0500

    Revert "Change Navbar Overhang"
    This reverts commit 73fba883fee015a741f7208f3e4839d3f994f3e7.

--- js/jquery.flexslider.js
+++ js/jquery.flexslider.js
@@ -1143,7 +1143,7 @@
     var scrollTop = $(window).scrollTop();
     var navbar = $('.navbar');
     
-    if (scrollTop < 800) {
+    if (scrollTop < 960) {
       navbar.addClass('navbar-hidden');
     } else {
       navbar.removeClass('navbar-hidden');
